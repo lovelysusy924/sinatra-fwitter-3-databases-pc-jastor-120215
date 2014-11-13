6c27a4a46cb67166400eb970e3268e2b9ae06985
vanessadean <vanessa.dean@gmail.com>
2014-11-13 14:47:20 -0500

    move to Sinatra base

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,12 @@
 require_relative "../../config/environment"
 require './app/models/tweet'
 
-class ApplicationController < Sinatra::Application
+class ApplicationController < Sinatra::Base
+
+  configure do
+    set :public_folder, 'public'
+    set :views, 'app/views'
+  end
 
   get '/tweets' do
     # Tweet.new("Vanessa", "My first tweet! SO EXCITING OMG!!!")
--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,4 @@
 require 'bundler'
 Bundler.require
 
-configure do
-  set :public_folder, 'public'
-  set :views, 'app/views'
-end
+
